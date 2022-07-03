bd3cb27f058b62e2152ec5d267ef8f535c955941
Hwaowoi <62416957+banhmikepxucxich@users.noreply.github.com>
2022-07-03 13:58:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas
+
+data = {
+  'series_1': [1, 3, 4, 5, 7],
+  'series_2': [10, 30, 40, 100, 250]
+}
+
+df = pandas.DataFrame(data)
 
 st.title('toast')
 st.subheader('steamlit + toast = the steam lit the toast')
 st.write('nice toast bro')
+st.write(df)
+st.line_chart(df)
